730e1ded84d2d73954203cb2cf5d7a753de8e8e5
Eli Wendlick <ewendlick@gmail.com>
2021-09-12 15:54:34 +0900

    Adds playground code for color-changing sidebar based on time

---
+++ playground/colorTransition.js
@@ -0,0 +1,29 @@
+'use strict'
+const COLOR_CHANGE_RATE = 10 // seconds
+
+const observer = new MutationObserver((_, instance) => {
+  const sideBarSelector = document.querySelector('.side-bar')
+  if (sideBarSelector) {
+    start(sideBarSelector)
+    instance.disconnect() // stop observing
+    return
+  }
+})
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+})
+
+const setColor = (selector) => {
+    const secondsFromEpoch = Math.round(new Date().getTime() / 1000)
+    const hue = Math.round(secondsFromEpoch / COLOR_CHANGE_RATE) % 360
+    selector.style.backgroundColor = `hsl(${hue}, 77%, 86%)`
+}
+
+const start = (selector) => {
+    setColor(selector)
+    setInterval(() => {
+        setColor(selector)
+    }, COLOR_CHANGE_RATE * 1000)
+}
